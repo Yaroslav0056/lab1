f385930d0fcb0adf5d5766ec4325999a69eae632
Yaroslav Miroshnichenko <miroshnichenko.jarik04@gmail.com>
2024-09-11 20:47:25 +0300

    add files

---
+++ lab1/part1.py
@@ -0,0 +1,10 @@
+city = 'Lviv'
+print(city)
+
+is_voter = False
+is_citizen = True
+print(is_citizen == is_voter)
+
+is_raining = False
+is_snowing = True
+print(is_snowing != is_raining)
---
+++ lab1/part2.py
@@ -0,0 +1,8 @@
+import math
+
+y = 7.315
+z = 3.127
+
+result = 16 * (y**2) + math.exp(y * z) + (z + 1.51)**(1/3) + math.log(y * z)
+
+print(result)
